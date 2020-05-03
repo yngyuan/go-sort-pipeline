51acc2fb5469cdf5dc5073307cf0a796e6863d49
yngyuan <zedd.yuan@utexas.edu>
2020-05-03 18:14:48 -0500

    added file reader and writer

--- pipelinedemo/main.go
+++ pipelinedemo/main.go
@@ -3,13 +3,44 @@ package main
 import (
 	"fmt"
 	"go-sort-pipeline/pipeline"
+	"os"
 )
 
 func main()  {
+	const filename = "large.in"
+	const n = 10000000
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p := pipeline.RandomSource(n)
+	pipeline.WriterSink(file, p)
+
+	file, err = os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p = pipeline.ReaderSource(file)
+	count := 0
+	for v := range p {
+		if count >= 100 {
+			break
+		}
+		fmt.Println(v)
+		count ++
+	}
+
+}
+
+func mergeDemo()  {
 	p := pipeline.Merge(
 		pipeline.InMemSort(
-		pipeline.ArraySource(
-			3, 2, 6, 7, 4)),
+			pipeline.ArraySource(
+				3, 2, 6, 7, 4)),
 		pipeline.InMemSort(
 			pipeline.ArraySource(
 				7, 9, 0, 3, 1, 8, 16, 11)))
