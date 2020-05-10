6edb807f21c4bbdf8a236122a47c39974b0f4d77
yngyuan <zedd.yuan@utexas.edu>
2020-05-09 19:04:46 -0500

    deleted unused code

--- pipelinedemo/main.go
+++ pipelinedemo/main.go
@@ -37,16 +37,3 @@ func main()  {
 	}
 }
 
-func mergeDemo()  {
-	p := pipeline.Merge(
-		pipeline.InMemSort(
-			pipeline.ArraySource(
-				3, 2, 6, 7, 4)),
-		pipeline.InMemSort(
-			pipeline.ArraySource(
-				7, 9, 0, 3, 1, 8, 16, 11)))
-
-	for v := range p {
-		fmt.Println(v)
-	}
-}
